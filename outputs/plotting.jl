3ae6fa8dc5c31b9206c65ef392848a72db973508
Josef Bisits <jbisits@gmail.com>
2026-03-04 21:29:17 +1100

    Fixed forcing files and modify expt setup

--- outputs/plotting.jl
+++ outputs/plotting.jl
@@ -15,14 +15,14 @@ Revise.includet("generate_tub_ics.jl")
 
 @info "Plotting and analysis environment setup!"
 
-expts = ("zstar-PPMH3", "hycom-PPMH3")
+expts = ("zstar-PPMH3", "hycom-PPMH3-wrongic")
 
 catalogue = Dict{String, Any}()
 
 for expt ∈ expts
 
     expt_path = joinpath(pwd(), "anu-tub-nm-" * expt)
-    odir = expt == "hycom-PPMH3" ? joinpath(expt_path, "output001") : joinpath(expt_path, "output442")
+    odir = joinpath(expt_path, "output442")
     daily = joinpath(odir, "ocean_daily.nc")
     monthly = joinpath(odir, "ocean_month.nc")
     monthlyz = joinpath(odir, "ocean_month_z.nc")
