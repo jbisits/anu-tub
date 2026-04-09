8ec2c6bf884e2f128d64c8a8e2545b0c75e3161d
Josef Bisits <jbisits@gmail.com>
2026-04-09 11:40:34 +1000

    Forgot the plotting script

--- outputs/plotting.jl
+++ outputs/plotting.jl
@@ -15,33 +15,35 @@ Revise.includet("generate_tub_ics.jl")
 
 @info "Plotting and analysis environment setup!"
 
-expts = ("zstar-PPMH3", "hycom-PPMH3")
+expts = ("zstar-PPMH3", "hycom-default-PPMH3", "hycom-dz3-PPMH3")
+output_dir = ("output442", "output000", "output002")
 
 catalogue = Dict{String, Any}()
 
-for expt ∈ expts
+for (i, expt) ∈ enumerate(expts)
 
-    expt_path = joinpath(pwd(), "anu-tub-nm-" * expt)
-    odir = expt == "zstar-PPMH3" ? joinpath(expt_path, "output442") : joinpath(expt_path, "output000")
-    daily = joinpath(odir, "ocean_daily.nc")
-    monthly = joinpath(odir, "ocean_month.nc")
-    monthlyz = joinpath(odir, "ocean_month_z.nc")
-    monthlyrho2 = joinpath(odir, "ocean_month_rho2.nc")
-    static = joinpath(odir, "ocean_static.nc")
-    vc = joinpath(odir, "Vertical_coordinate.nc")
+   expt_path = joinpath(pwd(), "anu-tub-nm-" * expt)
+   odir = joinpath(expt_path, output_dir[i])
+   daily = joinpath(odir, "ocean_daily.nc")
+   monthly = joinpath(odir, "ocean_month.nc")
+   monthlyz = joinpath(odir, "ocean_month_z.nc")
+   monthlyrho2 = joinpath(odir, "ocean_month_rho2.nc")
+   static = joinpath(odir, "ocean_static.nc")
+   vc = joinpath(odir, "Vertical_coordinate.nc")
 
-    catalogue[expt] = Dict("path" => expt_path,
-                           "daily" => daily,
-                           "monthly" => monthly,
-                           "monthlyrho2" => monthlyrho2,
-                           "monthlyz" => monthlyz,
-                           "static" => static, 
-                           "vc" => vc)
+   catalogue[expt] = Dict("path" => expt_path,
+                          "daily" => daily,
+                          "monthly" => monthly,
+                          "monthlyrho2" => monthlyrho2,
+                          "monthlyz" => monthlyz,
+                          "static" => static, 
+                          "vc" => vc)
 end
 
 @info "Experiments in the catalogue are:
        - $(expts[1])
        - $(expts[2])
+       - $(expts[3])
        Available data is:
           - daily
           - monthly
