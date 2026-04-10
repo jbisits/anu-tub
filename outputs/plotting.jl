fa24a0f7f204be2d4cb9f4ba55f2dbc0859051ef
Josef Bisits <jbisits@gmail.com>
2026-04-10 21:20:04 +1000

    Numerical mixing on diagnostic z grid

--- outputs/plotting.jl
+++ outputs/plotting.jl
@@ -16,7 +16,7 @@ Revise.includet("generate_tub_ics.jl")
 @info "Plotting and analysis environment setup!"
 
 expts = ("zstar-PPMH3", "hycom-default-PPMH3", "hycom-dz3-PPMH3")
-output_dir = ("output444", "output000", "output003")
+output_dir = ("output445", "output004", "output005")
 
 catalogue = Dict{String, Any}()
 
