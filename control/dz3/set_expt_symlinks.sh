9ef23ad2669aea47d9d1c2c3e7c2517b4daecf34
Josef Bisits <jbisits@gmail.com>
2026-05-25 13:47:08 +1000

    Update expts directory

---
+++ control/dz3/set_expt_symlinks.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -sfn ../common/diag_table diag_table
+ln -sfn ../common/input.nml input.nml
+
---
+++ control/dz3/sync_output_to_gdata.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -q normalbw
+#PBS -l ncpus=1
+#PBS -l wd
+#PBS -l storage=gdata/e14+scratch/e14
+#PBS -l walltime=5:00:00,mem=7GB
+#PBS -P e14
+#PBS -N output_to_gdata
+
+# Set this directory to something in /g/data3/hh5/tmp/cosima/
+# Make a unique path for your set of runs.
+# DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
+GDATADIR=/g/data/e14/jb2381/anu-tub/outputs/anu-tub-nm-dz3
+
+mkdir -p ${GDATADIR}
+cd archive
+rsync -vrltoD --safe-links output* ${GDATADIR}
+rsync -vrltoD --safe-links error_logs ${GDATADIR}
+rsync -vrltoD --safe-links pbs_logs ${GDATADIR}
+#rsync -vrltoD --safe-links restart* ${GDATADIR}
