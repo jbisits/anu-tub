2282eff29f22188b9ad15e763c4692b817427a4e
Josef Bisits <jbisits@gmail.com>
2026-06-01 11:06:22 +1000

    Update with the new experiment output paths

--- outputs/plotting.jl
+++ outputs/plotting.jl
@@ -15,8 +15,8 @@ Revise.includet("generate_tub_ics.jl")
 
 @info "Plotting and analysis environment setup!"
 
-expts = ("zstar-PPMH3", "hycom-default-PPMH3", "hycom-dz3-PPMH3")
-output_dir = ("output445", "output004", "output005")
+expts = ("zstar", "hycom", "dz3")
+output_dir = ("output002", "output002", "output002")
 
 catalogue = Dict{String, Any}()
 
